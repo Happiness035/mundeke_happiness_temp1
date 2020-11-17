1e72c9ecc1fe657715eeb8d9203975d51f255c4b
Happiness035 <72760463+Happiness035@users.noreply.github.com>
2020-11-17 15:46:56 -0500

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
 while True:
-    print("Temperature (F) : ")
-    input.temperature(TemperatureUnit.FAHRENHEIT)
+    "Current Room Temperature:95°F-35°C"
+    print("Temperature (F) : " + input.temperature(TemperatureUnit.FAHRENHEIT) + " -  Temperature (C): " + input.temperature(TemperatureUnit.CELSIUS))
+    
--- main.ts
+++ main.ts
@@ -1,4 +1,4 @@
 while (true) {
-    console.log("Temperature (F) : ")
-    input.temperature(TemperatureUnit.Fahrenheit)
+    /** Current Room Temperature:95°F-35°C */
+    console.log("Temperature (F) : " + input.temperature(TemperatureUnit.Fahrenheit) + " -  Temperature (C): " + input.temperature(TemperatureUnit.Celsius))
 }
